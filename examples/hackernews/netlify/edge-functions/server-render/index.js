57f272d9887520fa355dd836389432cee9a332a0
Rob Stanford <me@robstanford.com>
2022-06-07 17:29:04 +0100

    just create require

--- examples/hackernews/netlify/edge-functions/server-render/index.js
+++ examples/hackernews/netlify/edge-functions/server-render/index.js
@@ -1,3 +1,6 @@
+import { createRequire } from "https://deno.land/std@0.130.0/node/module.ts";
+const require = createRequire(import.meta.url);
+
 export default async function render(request, context) {
   return new Response("Hello again!");
 }
