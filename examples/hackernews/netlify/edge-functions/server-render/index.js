1f76c80a7eaf17202d4dcc19096fbdebf2c575b2
Rob Stanford <me@robstanford.com>
2022-06-07 17:50:33 +0100

    remove createrequire

--- examples/hackernews/netlify/edge-functions/server-render/index.js
+++ examples/hackernews/netlify/edge-functions/server-render/index.js
@@ -1,5 +1,4 @@
 import { createRequire } from "https://deno.land/std@0.130.0/node/module.ts";
-const require = createRequire(import.meta.url);
 
 export default async function render(request, context) {
   return new Response("Hello again!");
