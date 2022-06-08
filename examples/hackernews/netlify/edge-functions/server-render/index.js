6efe5cb676e21f58b791ac6999876649a572bde6
Rob Stanford <me@robstanford.com>
2022-06-08 10:17:52 +0100

    try esbuild

--- examples/hackernews/netlify/edge-functions/server-render/index.js
+++ examples/hackernews/netlify/edge-functions/server-render/index.js
@@ -1,5 +1,5 @@
 import { createRequire } from "https://deno.land/std@0.130.0/node/module.ts";
-const require = createRequire(Deno.mainModule);
+const require = createRequire(import.meta.url);
 
 export default async function render(request, context) {
   return new Response("Hello again!");
